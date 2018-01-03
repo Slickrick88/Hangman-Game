3e78c07abb2c6245ad8fb306b572b4cd3fc616e6
Slickrick88 <rickablake@gmail.com>
2018-01-03 17:03:03 -0500

    take 10

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,26 @@
+var score = 0;
+var bandNames = ["incubus", "poison", "wu tang clan", "judas priest", "rage against the machine", "system of a down", 
+                "red hot chili pepers", "flock of seagals", "limp bizkit", "toadies" ];
+
+var losses = 0;
+var wordSelector;
+var wordsUsed=[];
+var correctGuesses =[];
+var displayed = [];
+var incorrectGuesses = [];
+var guessesRemaining = 0;
+var bandName;
+var imagePath = "assets/images/";
+
+// start the game
+document.onkeyup = function(){
+    startGame()
+}
 function startGame(){
+    //reset varables at round level
     correctGuesses = [];
     incorrectGuesses = [];
+    // call function to select a band
     var bandName =  selectWord();
     document.querySelector("#wins").innerHTML = "Current Score is: "+ score;
     document.querySelector("#lostGames").innerHTML = "Number of losses is: "+ losses; 
@@ -13,6 +33,7 @@ function selectWord(){
     var wordSelector = Math.floor(Math.random() * 10);
     var bandName = bandNames[wordSelector];
     var q = wordsUsed.indexOf(bandName);
+    //establish if band has been used before and if so pick another
     if(q=-1){
         wordsUsed.push(bandName);
         gameSetup(bandName);
@@ -37,20 +58,23 @@ function gameSetup(bandName){
 
 //starts each specific round of the game after the word has been selected
 function roundStart(bandName){
-    guessesRemaining = bandName.length + 5;
-    document.querySelector("#incorrectGuesses").innerHTML = "Previous Wrong Guesses: " + incorrectGuesses.join(', ');
+    guessesRemaining = bandName.length;
+    document.querySelector("#incorrectGuesses").innerHTML = incorrectGuesses.join(', ');
     document.querySelector("#guessesRemaining").innerHTML = "You Have  " + guessesRemaining + " Guess Remaining";
     document.onkeyup = function(event) { 
         var userInput = String.fromCharCode(event.keyCode).toLowerCase();
         //check to see if selected letter in in string
         var n=bandName.lastIndexOf(userInput);
-
+        // see if the user is out of guesses
         if (guessesRemaining < 1){
             losses++;
             document.querySelector("#lostGames").innerHTML = "Number of Losses is: " + losses;
             startGame();
+        //if guesses remain continue game
         } else {
+            //check to see if users letter selection is in the selected band name
             if(n >-1){
+                //letter seleted is in the word
                 for(i=0; i < n+1; i++) {
                 //give position in the array of matching chararter
                     if(userInput === bandName[i]){
@@ -58,17 +82,20 @@ function roundStart(bandName){
                         document.querySelector("#wordDisplayed").innerHTML = displayed.join(' '); 
                     }
                 }
+                //call function to see if the user has won
                 if (checkWin(bandName,displayed)===1){
                     score ++;
                     document.querySelector("#wins").innerHTML = "Current Score is: "+ score;
+                    document.querySelector("#correctBand").innerHTML = bandName;
+                    document.querySelector('#bandPhoto').src= imagePath + bandNames.indexOf(bandName) + ".jpg";
                     startGame();
                 }
-
-            } else{
+            //letter selected is not in the word
+            } else {
                 incorrectGuesses.push(userInput);
                 -- guessesRemaining;
                 document.querySelector("#guessesRemaining").innerHTML = "You Have  " + guessesRemaining + " Guess Remaining";
-                document.querySelector("#incorrectGuesses").innerHTML = "Previous Wrong Guesses: " + incorrectGuesses.join(', ');
+                document.querySelector("#incorrectGuesses").innerHTML = incorrectGuesses.join(', ');
             }
         }
     }
@@ -93,10 +120,8 @@ function checkWin(bandName,displayed){
         }
     }
     if(wordScore === check){
-        console.log('word score = check');
         return 1;
     } else {
-        console.log('word score <> check');
         return 2;
     }
 };
